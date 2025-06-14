b667d060b39130acafe8c32e30b54db282a0139a
Doug Gordon <gordononline@bigpond.com>
2025-06-14 09:34:46 +0700

    added printing of primes

--- encryption.py
+++ encryption.py
@@ -11,17 +11,29 @@ import numpy as np
 
 
 def prime(a, b):
+    # set up list
     data = []
+    # iterate over a and b
     for digit in range(a, b + 1):
+        # If digit in range >1
         if digit > 1:
+            # set is_prim to True
             is_prime = True
+            # limit the calculation to values gt 2 for prime numbers divisor
             for i in range(2, digit):
+                # check if mod of digit / i == 0 - which indicates a non prime value
                 if (digit % i) == 0:
                     is_prime = False
                     break
+                # append the value of digit to the list
             if is_prime:
                 data.append(digit)
     return data
 
 
-print(prime(3, 1000))
+def multi(data):
+    for index, element in enumerate(data):
+        print(f"Index: {index}, Element: {element}")
+
+
+multi(prime(2, 24))
